64c87de51bbd84f03596ca10433069ab2f4da5fa
Mark Shkuro <mark_shkuro300696@mail.ru>
2022-08-15 18:40:42 +0300

    step3.2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-#!/usr/bin/env nodecea
-const { Command } = require("commander");
 const _ = require('lodash');
+const file1 = {
+    "host": "hexlet.io",
+    "timeout": 50,
+    "proxy": "123.234.53.22",
+    "follow": false
+  };
+const file2 = {
+    "timeout": 20,
+    "verbose": true,
+    "host": "hexlet.io"
+    };
 
-const program = new Command();
+const genDiff = (file1, file2) => {
+  const file1Cp = _.clone(file1);
+  const file2Cp = _.clone(file2);
+  const key1 = Object.keys(file1Cp);
+  const key2 = Object.keys(file2Cp);
+  const sortedUnionKeys = _.sortBy(_.union(key1, key2));
+  const result = sortedUnionKeys.reduce((acc, key) => {
+    if(_.has(file1Cp, key) && _.has(file2Cp, key) && file1[key] === file2[key]) acc.push(`  ${key} : ${file1Cp[key]}`);
+    else if(!_.has(file2Cp, key)) acc.push(`- ${key} : ${file1Cp[key]}`);
+    else if(_.has(file1Cp, key) && _.has(file2Cp, key) && file1[key] !== file2[key]) {
+      acc.push(`- ${key} : ${file1Cp[key]}`);
+      acc.push(`+ ${key} : ${file2Cp[key]}`);
+    }
+    else acc.push(`+ ${key} : ${file2Cp[key]}`)
+    return acc;
+  },[]);
+  return result.join(`\n`);
+};
 
-program
-    .version('0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format <type>', 'output format')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .parse(process.argv);
-    
- 
-console.log('Compares two configuration files and shows a difference.');
+console.log(genDiff(file1, file2));
