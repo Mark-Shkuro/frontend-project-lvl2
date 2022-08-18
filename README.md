476750e0ca84ea0f5e4384aaca6a92bfaaf24c31
Mark Shkuro <mark_shkuro300696@mail.ru>
2022-08-18 18:46:42 +0300

    step 4.4 codeclimate

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Mark-Shkuro/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Mark-Shkuro/frontend-project-lvl2/actions)
 
+<a href="https://codeclimate.com/github/Mark-Shkuro/frontend-project-lvl2/maintainability"><img src="https://api.codeclimate.com/v1/badges/872900f6052a226a0c0a/maintainability" /></a>
+
 <a href="https://asciinema.org/a/515230" target="_blank"><img src="https://asciinema.org/a/515230.svg" /></a>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import fs from 'fs';
 import path from 'path';
 
 const prepareFile = (file) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), file)));
-
+const copyKeys = (file1, file2) => _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
 const diffLogic = (file1, file2) => {
   const file1Cp = _.clone(file1);
   const file2Cp = _.clone(file2);
-  const key1 = Object.keys(file1Cp);
-  const key2 = Object.keys(file2Cp);
-  const sortedUnionKeys = _.sortBy(_.union(key1, key2));
+  const sortedUnionKeys = copyKeys(file1Cp, file2Cp);
   const result = sortedUnionKeys.reduce((acc, key) => {
     if (_.has(file1Cp, key) && _.has(file2Cp, key) && file1[key] === file2[key]) acc.push(`  ${key} : ${file1Cp[key]}`);
     else if (!_.has(file2Cp, key)) acc.push(`- ${key} : ${file1Cp[key]}`);
