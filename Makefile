966434d27758cf6ae3a7267d063423156526afea
Mark Shkuro <mark_shkuro300696@mail.ru>
2022-08-20 16:37:47 +0300

    step 4.8 test-coverage

--- Makefile
+++ Makefile
@@ -4,4 +4,6 @@ lint:
 	npx eslint .
 test: 
 	NODE_OPTIONS=--experimental-vm-modules npx jest
+test-coverage:
+	NODE_OPTIONS=--experimental-vm-modules npx jest --coverage
 .PHONY: test
--- src/index.js
+++ src/index.js
@@ -4,22 +4,28 @@ import path from 'path';
 
 const prepareFile = (file) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), file)));
 const copyKeys = (file1, file2) => _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
-const diffLogic = (file1, file2) => {
-  const file1Cp = _.clone(file1);
-  const file2Cp = _.clone(file2);
-  const sortedUnionKeys = copyKeys(file1Cp, file2Cp);
-  const result = sortedUnionKeys.reduce((acc, key) => {
-    if (_.has(file1Cp, key) && _.has(file2Cp, key) && file1[key] === file2[key]) acc.push(`  ${key} : ${file1Cp[key]}`);
-    else if (!_.has(file2Cp, key)) acc.push(`- ${key} : ${file1Cp[key]}`);
-    else if (_.has(file1Cp, key) && _.has(file2Cp, key) && file1[key] !== file2[key]) {
-      acc.push(`- ${key} : ${file1Cp[key]}`);
-      acc.push(`+ ${key} : ${file2Cp[key]}`);
-    } else acc.push(`+ ${key} : ${file2Cp[key]}`);
+
+const diffLogic = (file1, file2, keys) => {
+  const result = keys.reduce((acc, key) => {
+    if (_.has(file1, key) && _.has(file2, key) && file1[key] === file2[key]) acc.push(`  ${key} : ${file1[key]}`);
+    else if (!_.has(file2, key)) acc.push(`- ${key} : ${file1[key]}`);
+    else if (_.has(file1, key) && _.has(file2, key) && file1[key] !== file2[key]) {
+      acc.push(`- ${key} : ${file1[key]}`);
+      acc.push(`+ ${key} : ${file2[key]}`);
+    } else acc.push(`+ ${key} : ${file2[key]}`);
     return acc;
   }, []);
   return result.join('\n');
 };
 
-const genDiff = (path1, path2) => diffLogic(prepareFile(path1), prepareFile(path2));
+const checkDiff = (file1, file2) => {
+  const file1Cp = _.clone(file1);
+  const file2Cp = _.clone(file2);
+  const sortedUnionKeys = copyKeys(file1Cp, file2Cp);
+  const result = diffLogic(file1Cp, file2Cp, sortedUnionKeys);
+  return result;
+};
+
+const genDiff = (path1, path2) => checkDiff(prepareFile(path1), prepareFile(path2));
 
 export default genDiff;
